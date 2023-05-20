d83db67eb47d00c5d43f9648ce71cf1187ae1ac5
TheKingOfCSS <125829815+TheKingOfCSS@users.noreply.github.com>
2023-05-20 16:31:59 +0400

    Update main.js

--- sliders/main.js
+++ sliders/main.js
@@ -118,6 +118,8 @@ version.forEach((item, index) => {
 			setTimeout(() => window.location.replace('interestingSlider13/interest.html'), 1000);
 		} else if (index == 90) {
 			setTimeout(() => window.location.replace('iSlider2/interest.html'), 1000);
+		} else if (index == 91) {
+			setTimeout(() => window.location.replace('wonderfulParallaxSlider/wonderful.html'), 1000);
 		}
 	};
 });
